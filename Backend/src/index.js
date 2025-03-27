33bd579450a1dc46c70a56bdf3a32a066869126a
fardeen077 <fardeen36143@gmail.com>
2025-03-28 00:36:31 +0530

    initialize frontend and backend structure with Vite, React, and Express

---
+++ Backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config(); // Load environment variables
+const app = express();
+const PORT = process.env.PORT || 5001;
+// ✅ Middleware should come before routes
+app.use(express.json()); // Enable JSON parsing
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`⚙️ Server is running at port : ${PORT}`);
+    connectDB();
+});
