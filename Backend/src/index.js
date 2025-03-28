1d483ddd1ee8209865a8dcd4f661d01a84ca96c4
fardeen077 <fardeen36143@gmail.com>
2025-03-28 07:52:37 +0530

    implement authentication features: add login, logout, and profile update functionality; integrate Cloudinary for profile picture uploads; add middleware for route protection

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config(); // Load environment variables
 const app = express();
 const PORT = process.env.PORT || 5001;
 // ✅ Middleware should come before routes
 app.use(express.json()); // Enable JSON parsing
-
+app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
